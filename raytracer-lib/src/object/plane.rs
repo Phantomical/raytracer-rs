0a7423c1a418e943bd4c1210c1d60e028e40ea9f
swlynch <swlynch@uwaterloo.ca>
2017-12-24 15:11:40 -0500

    Implemented plane object

---
+++ raytracer-lib/src/object/plane.rs
@@ -0,0 +1,31 @@
+
+use vec::*;
+use object::*;
+
+pub struct Plane {
+	/// The normal vector of the plane.
+	/// All vectors laying on the plane
+	/// are perpendicular to this vector.
+	pub normal : Vec3d,
+	// A point on the plane
+	pub point  : Vec3d
+}
+
+impl Plane {
+	pub fn new(normal : Vec3d, point : Vec3d) -> Plane {
+		return Plane {
+			normal: normal.normalize(),
+			point:  point 
+		};
+	}
+}
+
+impl Raymarchable for Plane {
+	fn distance(&self, point : Vec3d) -> f64 {
+		return dot(self.normal, point - self.point);
+	}
+
+	fn normal_at(&self, _point : Vec3d, _dir : Vec3d) -> Vec3d {
+		return self.normal;
+	}
+}
