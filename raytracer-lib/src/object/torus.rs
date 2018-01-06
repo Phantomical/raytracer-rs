032dd146b01f0b6105514b562cff1fbe98f6feb3
swlynch <swlynch@uwaterloo.ca>
2018-01-06 16:30:05 -0500

    Added Torus

---
+++ raytracer-lib/src/object/torus.rs
@@ -0,0 +1,21 @@
+
+use lib::*;
+use lib::object::*;
+
+pub struct Torus {
+	inner_radius : f64,
+	outer_radius : f64
+}
+
+impl Torus {
+	pub fn new(inner_radius : f64, outer_radius : f64) -> Torus {
+		return Self { inner_radius, outer_radius };
+	}
+}
+
+impl Raymarchable for Torus {
+	fn distance(&self, point : Vec3d) -> f64 {
+		let q = Vec2d::new(point.xz().magnitude() - inner_radius, point.y);
+		return norm(q) - outer_radius;
+	}
+}
