51e70ab685b7c9e1ba6b0f9cee9feeb640afb622
Sean Lynch <swlynch@uwaterloo.ca>
2018-01-27 12:39:43 -0500

    Removed compilation results from source tree

--- gradient/target/debug/libgradient.d
+++
@@ -1,1 +0,0 @@
-D:\Projects\Projects\Rust\raytracer\gradient\target\debug\libgradient.rmeta: D:\Projects\Projects\Rust\raytracer\gradient\src\gradient.rs D:\Projects\Projects\Rust\raytracer\gradient\src\lib.rs
--- gradient/target/rls/debug/deps/gradient-95258f10afa966d8.d
+++
@@ -1,6 +0,0 @@
-D:\Projects\Projects\Rust\raytracer\gradient\target\rls\debug\deps\gradient-95258f10afa966d8.rmeta: src\lib.rs src\gradient.rs
-
-D:\Projects\Projects\Rust\raytracer\gradient\target\rls\debug\deps\gradient-95258f10afa966d8.d: src\lib.rs src\gradient.rs
-
-src\lib.rs:
-src\gradient.rs:
